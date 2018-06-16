8b87c8e777742429564e3d464365cc89117daed7
Rounder1 <tolerdavid@gmail.com>
2018-06-16 00:04:52 -0400

    added new db connection

--- server.js
+++ server.js
@@ -22,8 +22,13 @@ app.use(express.static("public"));
 
 // Require mongoose models
 var db = require("./models");
+// Save Connection as variable so that it can be used on heroku
+var connectdb = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoScraper");
+mongoose.connect(connectdb, function(){
+    if (err) console.log(err);
+    else console.log("Connected to db");
+});
 
 
 // ---Routes---
@@ -52,9 +57,6 @@ app.get("/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
       .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
       })
       .then(function(dbArticle) {res.json(dbArticle)})
