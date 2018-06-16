100eb6325e066c2860c61773a096341a3c711ba5
Rounder1 <tolerdavid@gmail.com>
2018-06-15 23:09:21 -0400

    Mongoose and Bootstrap setup

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-// Dependencies:
+// ---Dependencies---
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
-// var mongoose = require("mongoose");
-var mongojs = require("mongojs");
+var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var request = require("request");
+
+
+// ---Config Middleware---
 
 var PORT = process.env.PORT || 3000;
 var app = express();
@@ -17,27 +20,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the index.html page in the public folder
 app.use(express.static("public"));
 
-// Database configuration
-var databaseUrl = "mongoScraper";
-var collections = ["scrapedLinks"];
+// Require mongoose models
+var db = require("./models");
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/mongoScraper");
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+
+// ---Routes---
+
+// Return all data from database
+app.get("/articles", function(req, res){
+
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {res.json(dbArticle)})
+    .catch(function(err) {res.json(err)});
 });
 
-
-// Routes:
-
-// Get data from database
-app.get("/all", function(req, res){
-    db.scrapedLinks.find({}, function(err, data){
-        // Send error if there is a problam, else return data
-        if (err) console.log(err);
-        else res.json(data);
-    });
-});
+// Route to populate an article with it's note
+app.get("/articles/:id", function(req, res) {
+    
+    db.Article.findOne({ _id: req.params.id })
+      // Populate all of the notes associated with the selected article
+      .populate("note")
+      .then(function(dbArticle) {res.json(dbArticle)})
+      .catch(function(err) {res.json(err)});
+  });
+  
+  // Route for saving/updating an article's note
+  app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {res.json(dbArticle)})
+      .catch(function(err) {res.json(err)});
+  });
 
 // Scrape site for news
 app.get("/scrape", function(req, res){
@@ -51,30 +73,25 @@ app.get("/scrape", function(req, res){
         // Have cheerio find each a tag with class "storylink" in the html 
         $("a.storylink").each(function(i, element){
 
-            // Save the title and link of each story 
-            var title = $(element).text();
-            var link = $(element).attr("href");
+            // Create empty object
+            var result = {};
 
-            // If both were scraped successfully
-            if (title && link) {
-                // Insert into mongo database
-                db.scrapedLinks.insert({
-                    title: title, 
-                    link: link
-                },
-                function(err) {
-                    // Send error if there is a problam, else say it worked
-                    if (err) res.send(err);
-                    else res.send("it worked!");         
-                });
-            }
+            // Save the title and link of each story to the result object
+            result.title = $(this).text();
+            result.link = $(this).attr("href");
+
+            // Create new Article and push it to the database
+            db.Article.create(result)
+            .then(function(dbArticle) {console.log(dbArticle)})
+            .catch(function(err) {return res.json(err)});
         });
+        // return user to home page
+        res.send("Done!");
     });
 });
 
 
-
-// Start the server
+// ---Start the server---
 app.listen(PORT, function() {
     console.log("App running on port ", PORT);
 });
